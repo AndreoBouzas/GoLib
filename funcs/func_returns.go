d439e6ba02911c3f388f5fc90d14819382cd7ac6
Andreo <andreobouzas@gmail.com>
2021-12-01 14:02:42 -0300

    Adição do input de dados para o calculo

--- funcs/func_returns.go
+++ funcs/func_returns.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+//definindo as variáveis de input do usuário
+var num1, num2 int
+
 //criando a função de calculo que recebe dois valores "int" e retorna 4 valores "int" e 4 valores "String"
 func calc(x int, y int) (string, int, string, int, string, int, string, int) {
 	return "\nSOMA =", x + y, "\nSUBTRAÇÃO =", x - y, "\nMULTIPLICAÇÃO =", x * y, "\nDIVISÃO =", x / y
@@ -9,5 +12,14 @@ func calc(x int, y int) (string, int, string, int, string, int, string, int) {
 
 //chamando na func main a função de multiplo retorno criada
 func main() {
-	fmt.Println(calc(66, 33))
+	//solicitando ao usuário que digite o primeiro número
+	fmt.Println("Informe o primeiro número:")
+	//coletado o primeiro número digitado
+	fmt.Scanln(&num1)
+	//solicitando ao usuário que digite o segundo número
+	fmt.Println("Informe o Segundo número:")
+	//coletado o segundo número digitado
+	fmt.Scanln(&num2)
+	//dando print do resultado da função calc com os valores informados pelo usuário
+	fmt.Println(calc(num1, num2))
 }
